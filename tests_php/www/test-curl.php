d010c918bbaf52ffc86bd20b64770b0459f771a2
Mathieu Lecarme <mlecarme@bearstech.com>
2018-08-27 14:58:29 +0200

    More curl tests

--- tests_php/www/test-curl.php
+++ tests_php/www/test-curl.php
@@ -5,11 +5,14 @@ $ch = curl_init();
 // set URL and other appropriate options
 curl_setopt($ch, CURLOPT_URL, "https://bearstech.com/");
 curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
 
 // grab URL and pass it to the browser
-curl_exec($ch);
+$r = curl_exec($ch);
 
+if ($r != false) {
+    print('OK');
+}
 // close cURL resource, and free up system resources
 curl_close($ch);
 ?>
-//
