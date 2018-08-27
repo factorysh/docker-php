1ccfaaf6a9ac8a189810f5128f88f52091c346d1
Mathieu Lecarme <mlecarme@bearstech.com>
2018-08-27 14:35:47 +0200

    More tests

---
+++ tests_php/www/test-curl.php
@@ -0,0 +1,15 @@
+<?php
+// create a new cURL resource
+$ch = curl_init();
+
+// set URL and other appropriate options
+curl_setopt($ch, CURLOPT_URL, "https://bearstech.com/");
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+// grab URL and pass it to the browser
+curl_exec($ch);
+
+// close cURL resource, and free up system resources
+curl_close($ch);
+?>
+//
