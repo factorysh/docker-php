84cbe019374974e3d68a82e8649acf9647ea19cd
Mathieu Lecarme <mlecarme@bearstech.com>
2018-10-01 11:10:30 +0200

    msmtp in cli

---
+++ config_msmtp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ $MAILS_DOMAIN ]] && [[ $MAILS_USER ]] && [[ $MAILS_TOKEN ]]; then
+
+# set reasonable defaults
+
+SMTP_PORT=${MAILS_PORT:-25}
+SMTP_HOST=${MAILS_HOST:-mails}
+SMTP_FROM=${MAILS_FROM:-$MAILS_USER@$MAILS_DOMAIN}
+
+conf="
+defaults
+auth           plain
+tls            off
+logfile        /var/log/msmtp.log
+
+account        factory
+host           $SMTP_HOST
+port           $SMTP_PORT
+from           $SMTP_FROM
+user           $MAILS_USER
+password       $MAILS_TOKEN
+
+# Set a default account
+account default : factory
+"
+echo "$conf" > /etc/msmtprc
+
+fi
---
+++ entrypoint_cli.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/local/bin/config_msmtp.sh
+
+/bin/sh -c "$@"
