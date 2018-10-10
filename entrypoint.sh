202cafa9326875b62f2ed873e12fe4eb7e542aa6
Mathieu Lecarme <mlecarme@bearstech.com>
2018-10-10 18:13:21 +0200

    Fix: Private env

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 
 /usr/local/bin/config_msmtp.sh
 
-export WORKERS=${WORKERS:-5}
-export USER=$(id -u)
-envsubst < "/opt/www.conf.tpl" > "/etc/php/${PHP_VERSION}/fpm/pool.d/www.conf"
+WORKERS=${WORKERS:-5} USER=$(id -u) envsubst < "/opt/www.conf.tpl" > "/etc/php/${PHP_VERSION}/fpm/pool.d/www.conf"
 
 exec "$@"
