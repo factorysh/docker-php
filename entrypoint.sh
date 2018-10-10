dba3bc9ddd24c58a67b84d3ca24077b10cf15b83
Mathieu Lecarme <mlecarme@bearstech.com>
2018-10-10 20:24:58 +0200

    Fix: user with a name, not just an UID

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 /usr/local/bin/config_msmtp.sh
 
-WORKERS=${WORKERS:-5} USER=$(id -u) envsubst < "/opt/www.conf.tpl" > "/etc/php/${PHP_VERSION}/fpm/pool.d/www.conf"
+WORKERS=${WORKERS:-5} USER=$(id -un) envsubst < "/opt/www.conf.tpl" > "/etc/php/${PHP_VERSION}/fpm/pool.d/www.conf"
 
 exec "$@"
