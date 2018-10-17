f38222036de50d075c247bc1ee11c8679b179f11
Mathieu Lecarme <mlecarme@bearstech.com>
2018-10-17 15:25:47 +0200

    128M

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 /usr/local/bin/config_msmtp.sh
 
-WORKERS=${WORKERS:-5} SLOWLOGTIMEOUT=${SLOWLOGTIMEOUT:-0} TIMEOUT=${TIMEOUT:-30s} MEMORYLIMIT=${MEMORYLIMIT:-64M} USER=$(id -un) envsubst < "/opt/www.conf.tpl" > "/etc/php/${PHP_VERSION}/fpm/pool.d/www.conf"
+WORKERS=${WORKERS:-5} SLOWLOGTIMEOUT=${SLOWLOGTIMEOUT:-0} TIMEOUT=${TIMEOUT:-30s} MEMORYLIMIT=${MEMORYLIMIT:-128M} USER=$(id -un) envsubst < "/opt/www.conf.tpl" > "/etc/php/${PHP_VERSION}/fpm/pool.d/www.conf"
 
 exec "$@"
