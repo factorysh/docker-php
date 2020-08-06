721d902197e449191747eda3d3c5c703ce9a0466
Wilfried OLLIVIER <wollivier@bearstech.com>
2020-08-06 13:07:19 +0200

    Add entrypoint.d.sh script
    Adds a script handling multiple scripts in one directory with an example

---
+++ entrypoint.d.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="/entrypoint.d/"
+
+if [ ! -d "$DIR" ]
+then
+    echo "$DIR directory not found"
+    exit
+fi
+
+find "$DIR" -type f -executable | while read -r cmd
+do
+    echo "Executing $cmd"
+    bash -c "$cmd"
+done
