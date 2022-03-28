79f21fdb1dba34247a9c13873ee0449d9d3373e7
Emmanuel Mazurier <emmanuel.mazurier@gmail.com>
2022-03-28 15:18:23 +0200

    use temporary file for context array parsing

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -eux
 
 #get list of context from container : workers, max_spare_servers, start_servers
-context=($(php /usr/local/bin/workers.php))
+IFS=" " read -r -a context <<< "$(php /usr/local/bin/workers.php)"
 
 WORKERS=${context[0]} MAX_SPARE_SERVERS=${context[1]} START_SERVERS=${context[2]} SLOWLOGTIMEOUT=${SLOWLOGTIMEOUT:-0} TIMEOUT=${TIMEOUT:-0} MEMORYLIMIT=${MEMORYLIMIT:-128M} USER=$(id -un) envsubst < "/opt/www.conf.tpl" > "/etc/php/${PHP_VERSION}/fpm/pool.d/www.conf"
 
