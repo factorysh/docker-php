6ac3cabd7d273e221da6f7f7e9bdc873c96a6f0b
manumazu <emmanuel.mazurier@gmail.com>
2022-03-31 16:03:42 +0200

    max_spare_servers + cgroupv2 (#55)
    * manage cgroups version for memory and cpu limitation
    
    * simple list of context for workers, max_spare_servers, start_servers for fpm pool config
    
    * use temporary file for context array parsing
    
    * backport debian 10 + remove old ruby from makefile
    
    * disable update for Sury dependancies
    
    * disable tests for unmaintened images
    
    * harcode composer 2.2 for PHP7.0
    
    * manage composer versions for testing
    
    * manage cgroups version for memory and cpu limitation
    
    * simple list of context for workers, max_spare_servers, start_servers for fpm pool config
    
    * use temporary file for context array parsing
    
    * for testing after rebase

--- workers.php
+++ workers.php
@@ -16,10 +16,33 @@ $workers = getenv('WORKERS');
 if ($workers == '' ) {
 	$workers = 5;
 }
-if($workers == 'auto') {
+
+$max_spare_servers = getenv('MAX_SPARE_SERVERS');
+if($max_spare_servers == '') {
+	$max_spare_servers = 3;
+}
+
+$start_servers = getenv('START_SERVERS');
+if($start_servers == '') {
+	$start_servers = 2;
+}
+
+if(strtolower($workers) == 'auto') {
+
+	//which version for cgroup
+	$cgroupv2 = file_exists("/sys/fs/cgroup/cgroup.controllers");
+	$cg_mem_file_path = $cgroupv2 ? "/sys/fs/cgroup/memory.max" : 
+		"/sys/fs/cgroup/memory/memory.limit_in_bytes";
+	$cg_proc_file_path = $cgroupv2 ? "/sys/fs/cgroup/cpu.max" : 
+		"/sys/fs/cgroup/cpu/cpu.cfs_quota_us";
+	
 	$procs = 0;
-	$f = trim(file_get_contents("/sys/fs/cgroup/cpu/cpu.cfs_quota_us"));
-	if ($f != "-1") {
+	$f = trim(file_get_contents($cg_proc_file_path));
+	if($cgroupv2 && strstr($f, "max") === false) {
+		$t = explode($f, " ");
+		$procs = $t[0] / 100000; //need to be tested 
+	} 
+	else if (!$cgroupv2 && $f != "-1") {
 		$procs = $f / 100000;
 	} else {
 		$f = file_get_contents("/proc/cpuinfo");
@@ -29,7 +52,18 @@ if($workers == 'auto') {
 			}
 		}
 	}
-	$memory = trim(file_get_contents("/sys/fs/cgroup/memory/memory.limit_in_bytes"));
+
+	$memory = trim(file_get_contents($cg_mem_file_path));
+	if($cgroupv2 && $memory == "max") { // for cgroupv2
+		$f = file_get_contents("/proc/meminfo");
+		foreach (explode("\n", $f) as $line) {
+			if (strstr($line, 'MemTotal:')) {
+				preg_match('/\w+:\s+(\d+) (\w)B/', $line, $matches);
+				$memory = $matches[1] * $units[$matches[2]];
+			}
+		}
+	}
+
 	$workers = floor($memory / $ml);
 	// 2 is max worker per cpu
 	if ($workers > ($procs * 2)) {
@@ -38,7 +72,14 @@ if($workers == 'auto') {
 	if ($workers < 1) {
 		$workers = 1;
 	}
+
+	//workers should be greater than max_spare_servers
+	if($workers < $max_spare_servers) {
+		$max_spare_servers = 1;
+		$start_servers = 1;
+	}
 }
 
-print $workers;
+print $workers." ".$max_spare_servers." ".$start_servers;
+
 ?>
